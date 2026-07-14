8d60f450c05cba97a9f5a989685abc96b17dbd97
jesushernandezmonts <jesushernandezmonts@gmail.com>
2026-07-14 00:13:31 -0600

    fix: Remove duplicated transpilePackages entry in next.config.js

--- next.config.js
+++ next.config.js
@@ -4,6 +4,25 @@ const nextConfig = {
   // para evitar problemas con SSR o entornos de compilación.
   transpilePackages: ['date-fns', 'recharts'],
 
+
+  // Configuración explícita de Webpack para transpilar módulos específicos
+  webpack: (config, { isServer }) => {
+    // Asegurarse de que date-fns y recharts se transpilen
+    // Esto es un workaround para el "Module not found" persistente
+    config.module.rules.push({
+      test: /\.+(js|jsx|ts|tsx)$/,
+      include: [/node_modules\/date-fns/, /node_modules\/recharts/],
+      use: {
+        loader: 'babel-loader', // Usar babel-loader para transpilar
+        options: {
+          presets: ['next/babel'],
+          plugins: [],
+        },
+      },
+    });
+
+    return config;
+  },
 };
 
 module.exports = nextConfig;
