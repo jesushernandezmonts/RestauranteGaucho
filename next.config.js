c8df8eb9fe89f3af7b6d594bef1ecc220ee47eb7
jesushernandezmonts <jesushernandezmonts@gmail.com>
2026-07-14 00:08:28 -0600

    fix: Re-enable transpilePackages for date-fns and recharts in next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {
   // Asegúrate de que Recharts sea transpilado correctamente
   // para evitar problemas con SSR o entornos de compilación.
+  transpilePackages: ['date-fns', 'recharts'],
 
 };
 
